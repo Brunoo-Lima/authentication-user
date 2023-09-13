66ef258371f2e2ab2965c36eb10ca3a78d6992dd
Bruno Lima <bruno2.jean.lima@outlook.com>
2023-09-12 23:12:15 -0300

    feat: standardization message errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,26 @@
 //require('dotenv').config();
-import express from 'express';
+import express, { Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import { router } from './router';
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3000);
