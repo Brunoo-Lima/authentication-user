74936fce8c8fab3214827933a7ec9a26c69729d2
Bruno Lima <bruno2.jean.lima@outlook.com>
2023-09-13 00:03:07 -0300

    feat: authentication user and jwt token logging

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { CreateUserController } from './controllers/CreateUserController';
+import { AuthUserController } from './controllers/AuthUserController';
 
 const router = Router();
 
 router.post('/user', new CreateUserController().handle);
+router.post('/me', new AuthUserController().handle);
 
 export { router };
