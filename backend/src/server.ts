6956a22915bfd22b137f176f475207d3d8e927f5
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-11 21:36:23 -0300

    chore: update version lib

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,26 +1,6 @@
-require('dotenv').config();
-import 'express-async-errors';
-import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import { router } from './router';
+import 'dotenv/config';
+import { app } from './app';
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.use(router);
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof Error) {
-    return res.status(400).json({
-      error: err.message,
-    });
-  }
-
-  return res.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
-
-app.listen(3000, () => console.log('Servidor Online!'));
