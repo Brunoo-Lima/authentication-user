f13bbc06c12617e8f5a589639cfc6677f47bbf29
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-12 00:30:34 -0300

    feat: updating create user factory

--- backend/src/factories/controllers/user.ts
+++ backend/src/factories/controllers/user.ts
@@ -1,7 +1,13 @@
-import { IdGeneratorAdapter, PasswordHashAdapter } from '../../adapters';
+import {
+    EmailAdapter,
+    IdGeneratorAdapter,
+    PasswordHashAdapter,
+    TokenGeneratorAdapter,
+} from '../../adapters';
 import { CreateUserController } from '../../controllers';
 import {
     PostgresCreateUserRepository,
+    PostgresEmailVerificationRepository,
     PostgresGetUserByEmailRepository,
 } from '../../repositories/postgres';
 import { CreateUserUseCase } from '../../use-cases';
@@ -11,12 +17,19 @@ export const makeCreateUserController = () => {
     const getUserByEmailRepository = new PostgresGetUserByEmailRepository();
     const passwordHashAdapter = new PasswordHashAdapter();
     const idGeneratorAdapter = new IdGeneratorAdapter();
+    const tokenGeneratorAdapter = new TokenGeneratorAdapter();
+    const createEmailVerificationRepository =
+        new PostgresEmailVerificationRepository();
+    const emailAdapter = new EmailAdapter();
 
     const createUserUseCase = new CreateUserUseCase(
         getUserByEmailRepository,
         createUserRepository,
         passwordHashAdapter,
         idGeneratorAdapter,
+        tokenGeneratorAdapter,
+        createEmailVerificationRepository,
+        emailAdapter,
     );
 
     const createUserController = new CreateUserController(createUserUseCase);
