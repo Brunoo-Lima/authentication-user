16c58fb55e806d551ecd781bc91f3c31eaa75a70
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-11 23:23:19 -0300

    feat: add validation with zod

--- backend/src/controllers/user/create-user.ts
+++ backend/src/controllers/user/create-user.ts
@@ -2,6 +2,8 @@ import { Request } from 'express';
 import { ICreateUserUseCase } from '../../interfaces/use-cases';
 import { badRequest, created, serverError } from '../helpers';
 import { EmailAlreadyInUseError } from '../../errors';
+import { createUserSchema } from '../../schemas';
+import { ZodError } from 'zod';
 
 export class CreateUserController {
     private createUserUseCase: ICreateUserUseCase;
@@ -14,10 +16,16 @@ export class CreateUserController {
         try {
             const params = req.body;
 
+            await createUserSchema.parseAsync(params);
+
             const user = await this.createUserUseCase.execute(params);
 
             return created(user);
         } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({ message: error.issues[0]?.message });
+            }
+
             if (error instanceof EmailAlreadyInUseError) {
                 return badRequest({ message: error.message });
             }
