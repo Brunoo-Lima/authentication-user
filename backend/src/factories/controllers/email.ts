c234d1c3411f372820fb072e567fba0bfab258be
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-12 01:07:44 -0300

    feat: add factory verify email controller

---
+++ backend/src/factories/controllers/email.ts
@@ -0,0 +1,21 @@
+import { VerifyEmailController } from '../../controllers';
+import {
+    PostgresGetEmailVerificationByTokenRepository,
+    PostgresVerifyEmailRepository,
+} from '../../repositories/postgres';
+import { VerifyEmailUseCase } from '../../use-cases/email/verify-email';
+
+export const makeVerifyEmail = () => {
+    const getEmailVerificationByTokenRepository =
+        new PostgresGetEmailVerificationByTokenRepository();
+    const verifyEmailRepository = new PostgresVerifyEmailRepository();
+
+    const verifyEmailUseCase = new VerifyEmailUseCase(
+        getEmailVerificationByTokenRepository,
+        verifyEmailRepository,
+    );
+
+    const verifyEmailController = new VerifyEmailController(verifyEmailUseCase);
+
+    return verifyEmailController;
+};
