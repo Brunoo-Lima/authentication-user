5f717c3c7ce683eb4e0a10ee73f22536e5b34ef1
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-13 20:01:39 -0300

    feat: commenting on email verification code

--- api/src/factories/controllers/user.ts
+++ api/src/factories/controllers/user.ts
@@ -1,13 +1,7 @@
-import {
-    EmailAdapter,
-    IdGeneratorAdapter,
-    PasswordHashAdapter,
-    TokenGeneratorAdapter,
-} from '../../adapters';
+import { IdGeneratorAdapter, PasswordHashAdapter } from '../../adapters';
 import { CreateUserController } from '../../controllers';
 import {
     PostgresCreateUserRepository,
-    PostgresEmailVerificationRepository,
     PostgresGetUserByEmailRepository,
 } from '../../repositories/postgres';
 import { CreateUserUseCase } from '../../use-cases';
@@ -17,19 +11,19 @@ export const makeCreateUserController = () => {
     const getUserByEmailRepository = new PostgresGetUserByEmailRepository();
     const passwordHashAdapter = new PasswordHashAdapter();
     const idGeneratorAdapter = new IdGeneratorAdapter();
-    const tokenGeneratorAdapter = new TokenGeneratorAdapter();
-    const createEmailVerificationRepository =
-        new PostgresEmailVerificationRepository();
-    const emailAdapter = new EmailAdapter();
+    // const tokenGeneratorAdapter = new TokenGeneratorAdapter();
+    // const createEmailVerificationRepository =
+    //     new PostgresEmailVerificationRepository();
+    // const emailAdapter = new EmailAdapter();
 
     const createUserUseCase = new CreateUserUseCase(
         getUserByEmailRepository,
         createUserRepository,
         passwordHashAdapter,
         idGeneratorAdapter,
-        tokenGeneratorAdapter,
-        createEmailVerificationRepository,
-        emailAdapter,
+        // tokenGeneratorAdapter,
+        // createEmailVerificationRepository,
+        // emailAdapter,
     );
 
     const createUserController = new CreateUserController(createUserUseCase);
--- api/src/use-cases/user/create-user.ts
+++ api/src/use-cases/user/create-user.ts
@@ -1,44 +1,40 @@
 import { IUser } from '../../@types/IUser';
 import { EmailAlreadyInUseError } from '../../errors';
 import {
-    ICreateEmailVerificationRepository,
     ICreateUserRepository,
     IGetUserByEmailRepository,
 } from '../../interfaces/repositories';
 import {
-    IEmailAdapter,
     IIdGeneratorAdapter,
     IPasswordHashAdapter,
-    ITokenGeneratorAdapter,
 } from '../../interfaces/adapters';
-import { EMAIL_VERIFICATION_EXPIRY_MS } from '../../lib/email-verification-expiry';
 
 export class CreateUserUseCase {
     private getUserByEmailRepository: IGetUserByEmailRepository;
     private createUserRepository: ICreateUserRepository;
     private passwordHashAdapter: IPasswordHashAdapter;
     private idGeneratorAdapter: IIdGeneratorAdapter;
-    private tokenGeneratorAdapter: ITokenGeneratorAdapter;
-    private createEmailVerificationRepository: ICreateEmailVerificationRepository;
-    private emailAdapter: IEmailAdapter;
+    // private tokenGeneratorAdapter: ITokenGeneratorAdapter;
+    // private createEmailVerificationRepository: ICreateEmailVerificationRepository;
+    // private emailAdapter: IEmailAdapter;
 
     constructor(
         getUserByEmailRepository: IGetUserByEmailRepository,
         createUserRepository: ICreateUserRepository,
         passwordHashAdapter: IPasswordHashAdapter,
         idGeneratorAdapter: IIdGeneratorAdapter,
-        tokenGeneratorAdapter: ITokenGeneratorAdapter,
-        createEmailVerificationRepository: ICreateEmailVerificationRepository,
-        emailAdapter: IEmailAdapter,
+        // tokenGeneratorAdapter: ITokenGeneratorAdapter,
+        // createEmailVerificationRepository: ICreateEmailVerificationRepository,
+        // emailAdapter: IEmailAdapter,
     ) {
         this.getUserByEmailRepository = getUserByEmailRepository;
         this.createUserRepository = createUserRepository;
         this.passwordHashAdapter = passwordHashAdapter;
         this.idGeneratorAdapter = idGeneratorAdapter;
-        this.tokenGeneratorAdapter = tokenGeneratorAdapter;
-        this.createEmailVerificationRepository =
-            createEmailVerificationRepository;
-        this.emailAdapter = emailAdapter;
+        // this.tokenGeneratorAdapter = tokenGeneratorAdapter;
+        // this.createEmailVerificationRepository =
+        //     createEmailVerificationRepository;
+        // this.emailAdapter = emailAdapter;
     }
 
     async execute(user: IUser) {
@@ -62,16 +58,16 @@ export class CreateUserUseCase {
             password: hashedPassword,
         };
 
-        const token = this.tokenGeneratorAdapter.execute();
-        const expiresAt = new Date(Date.now() + EMAIL_VERIFICATION_EXPIRY_MS);
+        // const token = this.tokenGeneratorAdapter.execute();
+        // const expiresAt = new Date(Date.now() + EMAIL_VERIFICATION_EXPIRY_MS);
 
-        await this.createEmailVerificationRepository.execute(
-            userId,
-            token,
-            expiresAt,
-        );
+        // await this.createEmailVerificationRepository.execute(
+        //     userId,
+        //     token,
+        //     expiresAt,
+        // );
 
-        await this.emailAdapter.sendVerificationEmail(user.email, token);
+        // await this.emailAdapter.sendVerificationEmail(user.email, token);
 
         return await this.createUserRepository.execute(userData);
     }
