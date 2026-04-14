5659c5b1e4ae4b6a4af500a6368789e50ce59ac5
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-14 15:59:34 -0300

    feat: add factory and router refresh token controller

--- api/src/factories/controllers/auth.ts
+++ api/src/factories/controllers/auth.ts
@@ -1,8 +1,11 @@
-import { PasswordComparatorAdapter } from '../../adapters';
-import { TokensGeneratorAdapter } from '../../adapters/tokens-generator';
-import { LoginController } from '../../controllers';
+import {
+    PasswordComparatorAdapter,
+    TokensGeneratorAdapter,
+    TokenVerifierAdapter,
+} from '../../adapters';
+import { LoginController, RefreshTokenController } from '../../controllers';
 import { PostgresGetUserByEmailRepository } from '../../repositories/postgres';
-import { LoginUseCase } from '../../use-cases/auth/login';
+import { LoginUseCase, RefreshTokenUseCase } from '../../use-cases';
 
 export const makeLoginController = () => {
     const getUserByEmailRepository = new PostgresGetUserByEmailRepository();
@@ -17,4 +20,19 @@ export const makeLoginController = () => {
     const loginController = new LoginController(loginUseCase);
 
     return loginController;
+};
+
+export const makeRefreshTokenController = () => {
+    const tokensGeneratorAdapter = new TokensGeneratorAdapter();
+    const tokenVerifierAdapter = new TokenVerifierAdapter();
+    const refreshTokenUseCase = new RefreshTokenUseCase(
+        tokensGeneratorAdapter,
+        tokenVerifierAdapter,
+    );
+
+    const refreshTokenController = new RefreshTokenController(
+        refreshTokenUseCase,
+    );
+
+    return refreshTokenController;
 };
