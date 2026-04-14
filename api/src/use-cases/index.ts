56411399d8fef018259037f87114ae600f715390
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-14 11:43:46 -0300

    feat: add DeleteUserUseCase

--- api/src/use-cases/index.ts
+++ api/src/use-cases/index.ts
@@ -1,4 +1,5 @@
 export * from './user/create-user';
 export * from './user/get-user-by-id';
+export * from './user/delete-user';
 
 export * from './email/verify-email';
---
+++ api/src/use-cases/user/delete-user.ts
@@ -0,0 +1,13 @@
+import { IDeleteUserRepository } from '../../interfaces/repositories';
+
+export class DeleteUserUseCase {
+    private deleteUserRepository: IDeleteUserRepository;
+
+    constructor(deleteUserRepository: IDeleteUserRepository) {
+        this.deleteUserRepository = deleteUserRepository;
+    }
+
+    async execute(userId: string) {
+        return await this.deleteUserRepository.execute(userId);
+    }
+}
