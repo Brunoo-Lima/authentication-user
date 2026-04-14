3152144e1601e333100136f71f53796d92ed5c50
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-14 17:22:36 -0300

    feat: update router email

--- api/src/routes/email.ts
+++ api/src/routes/email.ts
@@ -3,11 +3,14 @@ import { makeVerifyEmail } from '../factories/controllers/email';
 
 const emailRoutes: IRouter = Router();
 
-emailRoutes.post('/verify', async (request: Request, response: Response) => {
+const handleVerifyEmail = async (request: Request, response: Response) => {
     const verifyEmailController = makeVerifyEmail();
     const { statusCode, body } = await verifyEmailController.execute(request);
 
     return response.status(statusCode).send(body);
-});
+};
+
+emailRoutes.get('/verify', handleVerifyEmail);
+// emailRoutes.post('/verify', handleVerifyEmail);
 
 export { emailRoutes };
