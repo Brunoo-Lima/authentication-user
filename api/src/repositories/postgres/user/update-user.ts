3f934f34dfdbb75ecc92b18840fe86d21628c16c
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-14 13:42:26 -0300

    feat: added error prisma for user not found return 404

--- api/src/repositories/postgres/user/update-user.ts
+++ api/src/repositories/postgres/user/update-user.ts
@@ -1,13 +1,26 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
 import { IUpdateUser } from '../../../@types/IUser';
 import { prisma } from '../../../lib/prisma';
+import { UserNotFoundError } from '../../../errors';
 
 export class PostgresUpdateUserRepository {
     async execute(userId: string, updateUserParams: IUpdateUser) {
-        return await prisma.user.update({
-            where: {
-                id: userId,
-            },
-            data: updateUserParams,
-        });
+        try {
+            return await prisma.user.update({
+                where: {
+                    id: userId,
+                },
+                data: updateUserParams,
+            });
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                // P2025 = An operation failed because it depends on one or more records that were required but not found. {cause}
+                if (error.code === 'P2025') {
+                    throw new UserNotFoundError(userId);
+                }
+            }
+
+            throw error;
+        }
     }
 }
