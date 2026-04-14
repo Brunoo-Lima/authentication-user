acfa57b4d0e7c7700853bb278548f096db6767dc
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-14 13:37:08 -0300

    feat: validation if email already exists on updateUserUseCase

--- api/src/use-cases/user/update-user.ts
+++ api/src/use-cases/user/update-user.ts
@@ -1,20 +1,38 @@
 import { IUpdateUser } from '../../@types/IUser';
+import { EmailAlreadyInUseError } from '../../errors';
 import { IPasswordHashAdapter } from '../../interfaces/adapters';
-import { IUpdateUserRepository } from '../../interfaces/repositories';
+import {
+    IGetUserByEmailRepository,
+    IUpdateUserRepository,
+} from '../../interfaces/repositories';
 
 export class UpdateUserUseCase {
     private updateUserRepository: IUpdateUserRepository;
+    private getUserByEmailRepository: IGetUserByEmailRepository;
     private passwordHashAdapter: IPasswordHashAdapter;
 
     constructor(
         updateUserRepository: IUpdateUserRepository,
+        getUserByEmailRepository: IGetUserByEmailRepository,
         passwordHashAdapter: IPasswordHashAdapter,
     ) {
         this.updateUserRepository = updateUserRepository;
+        this.getUserByEmailRepository = getUserByEmailRepository;
         this.passwordHashAdapter = passwordHashAdapter;
     }
 
     async execute(userId: string, updateUserParams: IUpdateUser) {
+        if (updateUserParams.email) {
+            const userAlreadyExists =
+                await this.getUserByEmailRepository.execute(
+                    updateUserParams.email,
+                );
+
+            if (userAlreadyExists && userAlreadyExists.id !== userId) {
+                throw new EmailAlreadyInUseError(updateUserParams.email);
+            }
+        }
+
         const user = { ...updateUserParams };
 
         if (updateUserParams.password) {
