459da1edcedf246d72e06276bbbb011ba971ab6a
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-15 17:20:55 -0300

    feat: add InvalidPasswordError or UserNotFoundError on login controller

--- api/src/controllers/auth/login.ts
+++ api/src/controllers/auth/login.ts
@@ -1,8 +1,13 @@
 import { type Request } from 'express';
 import { ILoginUseCase } from '../../interfaces/use-cases';
-import { badRequest, ok, serverError } from '../helpers';
+import { badRequest, ok, serverError, unauthorized } from '../helpers';
 import { loginSchema } from '../../schemas';
-import { EmailNotVerifiedError } from '../../errors';
+import {
+    EmailNotVerifiedError,
+    InvalidPasswordError,
+    UserNotFoundError,
+} from '../../errors';
+import { ZodError } from 'zod';
 
 export class LoginController {
     private loginUseCase: ILoginUseCase;
@@ -36,6 +41,17 @@ export class LoginController {
 
             if (error instanceof EmailNotVerifiedError) {
                 return badRequest({ message: error.message });
+            }
+
+            if (
+                error instanceof InvalidPasswordError ||
+                error instanceof UserNotFoundError
+            ) {
+                return unauthorized('Invalid credentials');
+            }
+
+            if (error instanceof ZodError) {
+                return badRequest({ message: error.issues[0].message });
             }
 
             return serverError();
