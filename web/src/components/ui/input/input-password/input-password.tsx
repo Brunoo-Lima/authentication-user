ad933a8f9bea676248f2952189fa41df6574a33f
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-17 17:16:15 -0300

    feat: separating reusable components

---
+++ web/src/components/ui/input/input-password/input-password.tsx
@@ -0,0 +1,79 @@
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
+import { forwardRef, useState } from 'react';
+import { FieldError } from 'react-hook-form';
+
+import s from './input.module.css';
+
+interface InputPasswordProps {
+  label?: string;
+  placeholder?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  containerClassName?: string;
+  inputClassName?: string;
+  value?: string;
+  error?: FieldError;
+  maxLength?: number;
+}
+
+export const InputPassword = forwardRef<HTMLInputElement, InputPasswordProps>(
+  (
+    {
+      label,
+      placeholder,
+      onChange,
+      containerClassName,
+      inputClassName,
+      value,
+      error,
+      maxLength,
+      ...rest
+    },
+    ref,
+  ) => {
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+
+    return (
+      <div className={`${s.input__container} ${containerClassName ?? ''}`}>
+        {label && (
+          <label className={s.label} htmlFor={label}>
+            {label}
+          </label>
+        )}
+
+        <div className={s.input__wrapper}>
+          <input
+            ref={ref}
+            id={label}
+            className={`${s.input__field} ${inputClassName ?? ''}`}
+            type={showPassword ? 'text' : 'password'}
+            placeholder={placeholder}
+            onChange={onChange}
+            value={value}
+            maxLength={maxLength}
+            {...rest}
+          />
+
+          {showPassword ? (
+            <EyeIcon
+              size={24}
+              color="#A4A7AB"
+              className={s.icon}
+              onClick={() => setShowPassword(false)}
+            />
+          ) : (
+            <EyeOffIcon
+              size={24}
+              color="#A4A7AB"
+              className={s.icon}
+              onClick={() => setShowPassword(true)}
+            />
+          )}
+        </div>
+
+        {error && <small className={s.error__text}>{error.message}</small>}
+      </div>
+    );
+  },
+);
+
+InputPassword.displayName = 'InputPassword';
---
+++ web/src/components/ui/input/input.tsx
@@ -0,0 +1,55 @@
+import { FieldError } from 'react-hook-form';
+import s from './input.module.css';
+
+interface IRootProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+  classNameCustom?: string;
+}
+
+export const Root = ({ children, classNameCustom }: IRootProps) => {
+  return (
+    <div className={`${s.input__container} ${classNameCustom}`}>{children}</div>
+  );
+};
+
+interface ILabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  classNameCustom?: string;
+}
+
+export const Label = ({ classNameCustom, ...props }: ILabelProps) => {
+  return <label className={`${s.label} ${classNameCustom}`} {...props} />;
+};
+
+interface IFormFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  classNameCustom?: string;
+}
+
+export const FormField = ({ classNameCustom, ...props }: IFormFieldProps) => {
+  return (
+    <input className={`${s.input__field} ${classNameCustom}`} {...props} />
+  );
+};
+
+interface IErrorMessageProps {
+  message: string | FieldError | undefined;
+  classNameCustom?: string;
+}
+
+export const ErrorMessage = ({
+  classNameCustom,
+  message,
+}: IErrorMessageProps) => {
+  return (
+    <small className={`${s.input__error} ${classNameCustom}`}>
+      {message as string}
+    </small>
+  );
+};
+
+interface IIconProps {
+  children: React.ReactNode;
+  classNameCustom?: string;
+}
+export const Icon = ({ children, classNameCustom }: IIconProps) => {
+  return <span className={classNameCustom}>{children}</span>;
+};
