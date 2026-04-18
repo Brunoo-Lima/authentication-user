dc4918e4a8e8f60030650b0a6d0481f904434ff6
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-18 14:28:48 -0300

    feat: add header

---
+++ web/src/components/header/header.tsx
@@ -0,0 +1,27 @@
+import { NavLink, useLocation } from 'react-router-dom';
+import s from './header.module.css';
+
+export const Header = () => {
+  const location = useLocation();
+
+  return (
+    <header className={s.header__wrapper}>
+      <h1>Logo</h1>
+
+      <nav className={s.nav}>
+        <NavLink
+          to="/inicio"
+          className={`${location.pathname === '/inicio' ? s.active : ''}`}
+        >
+          Inicio
+        </NavLink>
+        <NavLink
+          to="/me"
+          className={`${location.pathname === '/me' ? s.active : ''}`}
+        >
+          Meu perfil
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
