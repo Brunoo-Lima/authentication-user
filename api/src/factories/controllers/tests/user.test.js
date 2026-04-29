ebf5e77fb8b521aa2cda1595005ad1250710cb10
Bruno <Bruno2.jean.lima@outlook.com>
2026-04-29 15:59:04 -0300

    test: ensure UserController factories test

---
+++ api/src/factories/controllers/tests/user.test.js
@@ -0,0 +1,32 @@
+import {
+    makeCreateUserController,
+    makeDeleteUserController,
+    makeGetUserByIdController,
+    makeUpdateUserController,
+} from '../user';
+import {
+    CreateUserController,
+    DeleteUserController,
+    GetUserByIdController,
+    UpdateUserController,
+} from '../../../controllers';
+
+describe('User Controller Factory', () => {
+    it('should return a valid CreateUserController instance', () => {
+        expect(makeCreateUserController()).toBeInstanceOf(CreateUserController);
+    });
+
+    it('should return a valid GetUserByIdController instance', () => {
+        expect(makeGetUserByIdController()).toBeInstanceOf(
+            GetUserByIdController,
+        );
+    });
+
+    it('should return a valid UpdateUserController instance', () => {
+        expect(makeUpdateUserController()).toBeInstanceOf(UpdateUserController);
+    });
+
+    it('should return a valid DeleteUserController instance', () => {
+        expect(makeDeleteUserController()).toBeInstanceOf(DeleteUserController);
+    });
+});
